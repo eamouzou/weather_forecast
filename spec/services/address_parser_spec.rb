b1e5af58f767ec597ca6ee9f1c98eaeaea08d47f
eamouzou <elom1990@yahoo.com>
2025-03-05 15:35:32 -0700

    Add unit tests for AddressParser service

--- spec/services/address_parser_spec.rb
+++ spec/services/address_parser_spec.rb
@@ -1,74 +1,73 @@
+# spec/services/address_parser_spec.rb
 require 'rails_helper'
 
 RSpec.describe AddressParser do
+    let(:geocoding_service) { instance_double('GeocodingService') }
+  
+    before do
+        allow(GeocodingService).to receive(:new).and_return(geocoding_service)
+    end
+  
     describe '#parse' do
         context 'with valid addresses' do
-            it 'gets coordinates from a full address' do
-                VCR.use_cassette('geocoding/full_address') do
-                    parser = AddressParser.new
-                    result = parser.parse('123 Main St, New York, NY 10001')
-                    
-                    expect(result).to be_a(Hash)
-                    expect(result).to have_key(:lat)
-                    expect(result).to have_key(:lon)
-                    expect(result[:lat]).to be_within(0.1).of(40.7128)
-                    expect(result[:lon]).to be_within(0.1).of(-74.0060)
-                end
+            it 'delegates zip codes to geocode_zip method' do
+                parser = AddressParser.new
+                allow(geocoding_service).to receive(:geocode_zip).with('10001').and_return({lat: 40.7128, lon: -74.0060, zip_code: '10001'})
+                
+                result = parser.parse('10001')
+                
+                expect(geocoding_service).to have_received(:geocode_zip).with('10001')
+                expect(result).to be_a(Hash)
+                expect(result[:lat]).to eq(40.7128)
+                expect(result[:lon]).to eq(-74.0060)
+                expect(result[:zip_code]).to eq('10001')
             end
-      
-            it 'gets coordinates from a city and state' do
-                VCR.use_cassette('geocoding/city_state') do
-                    parser = AddressParser.new
-                    result = parser.parse('San Francisco, CA')
-                    
-                    expect(result).to be_a(Hash)
-                    expect(result).to have_key(:lat)
-                    expect(result).to have_key(:lon)
-                    expect(result[:lat]).to be_within(0.1).of(37.7749)
-                    expect(result[:lon]).to be_within(0.1).of(-122.4194)
-                end
+        
+            it 'handles zip codes with whitespace' do
+                parser = AddressParser.new
+                allow(geocoding_service).to receive(:geocode_zip).with('10001').and_return({lat: 40.7128, lon: -74.0060, zip_code: '10001'})
+                
+                result = parser.parse('  10001  ')
+                
+                expect(geocoding_service).to have_received(:geocode_zip).with('10001')
             end
-      
-            it 'extracts coordinates from a zip code' do
-                VCR.use_cassette('geocoding/zip_code') do
-                    parser = AddressParser.new
-                    result = parser.parse('10001')
-                    
-                    expect(result).to be_a(Hash)
-                    expect(result).to have_key(:lat)
-                    expect(result).to have_key(:lon)
-                    expect(result[:lat]).to be_a(Numeric)
-                    expect(result[:lon]).to be_a(Numeric)
-                    expect(result).to have_key(:zip_code)
-                    expect(result[:zip_code]).to eq('10001')
-                end
+        
+            it 'delegates text addresses to geocode_address method' do
+                parser = AddressParser.new
+                allow(geocoding_service).to receive(:geocode_address).with('New York, NY').and_return({lat: 40.7128, lon: -74.0060})
+                
+                result = parser.parse('New York, NY')
+                
+                expect(geocoding_service).to have_received(:geocode_address).with('New York, NY')
+                expect(result).to be_a(Hash)
+                expect(result[:lat]).to eq(40.7128)
+                expect(result[:lon]).to eq(-74.0060)
+            end
+        
+            it 'delegates full addresses with zip code to geocode_address method' do
+                parser = AddressParser.new
+                allow(geocoding_service).to receive(:geocode_address).with('123 Main St, New York, NY 10001').and_return({lat: 40.7128, lon: -74.0060})
+                
+                result = parser.parse('123 Main St, New York, NY 10001')
+                
+                expect(geocoding_service).to have_received(:geocode_address).with('123 Main St, New York, NY 10001')
             end
         end
-    
+        
         context 'with invalid addresses' do
-            it 'raises an error for nonsensical input' do
-                VCR.use_cassette('geocoding/nonsense') do
-                    parser = AddressParser.new
-
-                    expect { parser.parse('xyzabc123$$$') }.to raise_error(StandardError, /Invalid address/)
-                end
+            it 'raises an error for nil input' do
+                parser = AddressParser.new
+                expect { parser.parse(nil) }.to raise_error(StandardError, /Address cannot be blank/)
             end
             
-            it 'raises an error for empty input' do
+            it 'raises an error for empty string input' do
                 parser = AddressParser.new
-
                 expect { parser.parse('') }.to raise_error(StandardError, /Address cannot be blank/)
             end
             
-            it 'raises an error when geocoding fails' do
-                VCR.use_cassette('geocoding/service_failure') do
-                    parser = AddressParser.new
-                    allow_any_instance_of(HTTParty::Response).to receive(:success?).and_return(false)
-                    allow_any_instance_of(HTTParty::Response).to receive(:code).and_return(500)
-                    allow_any_instance_of(HTTParty::Response).to receive(:message).and_return('Internal Server Error')
-                    
-                    expect { parser.parse('New York') }.to raise_error(StandardError, /Geocoding API Error/)
-                end
+            it 'raises an error for whitespace-only input' do
+                parser = AddressParser.new
+                expect { parser.parse('   ') }.to raise_error(StandardError, /Address cannot be blank/)
             end
         end
     end
