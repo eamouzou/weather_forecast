c434d019ce622049a187643baefd459a3e98e66b
eamouzou <elom1990@yahoo.com>
2025-03-05 10:32:57 -0700

    Add test for extended forecast

--- spec/services/weather_service_spec.rb
+++ spec/services/weather_service_spec.rb
@@ -1,18 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe WeatherService do
-  describe '#get_current_temperature' do
-    it 'fetches current temperature for given coordinates' do
-      VCR.use_cassette('openweathermap/current_weather') do
-        service = WeatherService.new
-        result = service.get_current_temperature(lat: 40.7128, lon: -74.0060)
-        
-        expect(result).to be_a(Hash)
-        expect(result).to have_key(:temperature)
-        expect(result[:temperature]).to be_a(Numeric)
-        expect(result).to have_key(:from_cache)
-        expect(result[:from_cache]).to be(false)
-      end
+    describe '#get_current_temperature' do
+        it 'fetches current temperature for given coordinates' do
+            VCR.use_cassette('openweathermap/current_weather') do
+                service = WeatherService.new
+                result = service.get_current_temperature(lat: 40.7128, lon: -74.0060)
+                
+                expect(result).to be_a(Hash)
+                expect(result).to have_key(:temperature)
+                expect(result[:temperature]).to be_a(Numeric)
+                expect(result).to have_key(:from_cache)
+                expect(result[:from_cache]).to be(false)
+            end
+        end
     end
-  end
+    
+    
+    describe '#get_forecast' do
+        it 'fetches extended forecast for given coordinates' do
+            VCR.use_cassette('openweathermap/forecast') do
+                service = WeatherService.new
+                result = service.get_forecast(lat: 40.7128, lon: -74.0060)
+                
+                expect(result).to be_a(Hash)
+                expect(result).to have_key(:daily_forecast)
+                expect(result[:daily_forecast]).to be_an(Array)
+                expect(result).to have_key(:from_cache)
+                expect(result[:from_cache]).to be(false)
+            end
+        end
+    end
 end
