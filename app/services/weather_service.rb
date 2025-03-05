ed21ef38715df3f15f0e363f4f41146aad74a9ea
eamouzou <elom1990@yahoo.com>
2025-03-05 10:15:49 -0700

    Add current temp method with api call

--- app/services/weather_service.rb
+++ app/services/weather_service.rb
@@ -3,4 +3,17 @@ class WeatherService
       @base_url = Rails.application.config.weather_api[:base_url]
       @api_key = Rails.application.config.weather_api[:api_key]
     end
+
+    def get_current_temperature(lat:, lon:)
+        # Call API
+        HTTParty.get(
+          "#{@base_url}/weather",
+          query: {
+            lat: lat,
+            lon: lon,
+            appid: @api_key,
+            units: 'imperial'
+          }
+        )
+    end
   end
