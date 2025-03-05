ba4a56ed88c1209cf1b260ec1d665b0037c5ecd2
eamouzou <elom1990@yahoo.com>
2025-03-05 10:04:22 -0700

    Create weather service test

---
+++ spec/services/weather_service_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe WeatherService do
+  describe '#get_current_temperature' do
+    it 'fetches current temperature for given coordinates' do
+      VCR.use_cassette('openweathermap/current_weather') do
+        service = WeatherService.new
+        result = service.get_current_temperature(lat: 40.7128, lon: -74.0060)
+        
+        expect(result).to be_a(Hash)
+        expect(result).to have_key(:temperature)
+        expect(result[:temperature]).to be_a(Numeric)
+      end
+    end
+  end
+end
