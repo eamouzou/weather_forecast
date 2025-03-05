45c82fd76b2418fe832ae4ca87e9bb4233ecf95d
eamouzou <elom1990@yahoo.com>
2025-03-05 11:49:08 -0700

    Make get current temp more modular

--- app/services/weather_service.rb
+++ app/services/weather_service.rb
@@ -1,30 +1,37 @@
 class WeatherService
     def initialize
-      @base_url = Rails.application.config.weather_api[:base_url]
-      @api_key = Rails.application.config.weather_api[:api_key]
+        @base_url = Rails.application.config.weather_api[:base_url]
+        @api_key = Rails.application.config.weather_api[:api_key]
     end
 
     def get_current_temperature(lat:, lon:)
-        # Call API
+        response = make_api_request("weather", lat: lat, lon: lon)
+        
+        {
+            temperature: response['main']['temp'],
+            from_cache: false
+        }
+    end      
+      
+    private
+      
+    def make_api_request(endpoint, lat:, lon:)
         response = HTTParty.get(
-          "#{@base_url}/weather",
-          query: {
-            lat: lat,
-            lon: lon,
-            appid: @api_key,
-            units: 'imperial'
-          }
-        )
-
-        parse_response(response)
-    end
-
-    def parse_response(response)
-        if response.success?
-            {
-              temperature: response['main']['temp'],
-              from_cache: false
+            "#{@base_url}/#{endpoint}",
+            query: {
+                lat: lat,
+                lon: lon,
+                appid: @api_key,
+                units: 'imperial'
             }
+        )
+        
+        handle_response(response)
+    end
+    
+    def handle_response(response)
+        if response.success?
+            response
         else
             raise "API Error: #{response.code} - #{response.message}"
         end
