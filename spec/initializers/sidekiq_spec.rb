ee0c4a7b019f1dd42601f654ad6d6c3f1d6339a0
eamouzou <elom1990@yahoo.com>
2025-03-06 12:33:20 -0700

    Modify Sidekiq spec to test config

--- spec/initializers/sidekiq_spec.rb
+++ spec/initializers/sidekiq_spec.rb
@@ -1,26 +1,38 @@
 # spec/initializers/sidekiq_spec.rb
-
 require 'rails_helper'
 require 'sidekiq'
 
 RSpec.describe "Sidekiq Configuration" do
-  before do
-    # Stub environment variables
-    allow(ENV).to receive(:fetch).with('REDIS_URL', anything).and_return('redis://test-redis:6379/1')
-    allow(ENV).to receive(:fetch).with('SIDEKIQ_REDIS_POOL_SIZE', anything).and_return('10')
-  end
+  context "when no custom Redis URL is set" do
+    before do
+      ENV.delete('REDIS_URL')
+      ENV.delete('SIDEKIQ_REDIS_POOL_SIZE')
+    end
 
-  it "configures server redis correctly" do
-    Sidekiq.configure_server do |config|
-      expect(config.redis[:url]).to eq('redis://test-redis:6379/1')
-      expect(config.redis[:size]).to eq(10)
+    it "uses default Redis configuration" do
+      Sidekiq.configure_server do |config|
+        expect(config.redis[:url]).to eq('redis://localhost:6379/1')
+        expect(config.redis[:size]).to eq(5)
+      end
     end
   end
 
-  it "configures client redis correctly" do
-    Sidekiq.configure_client do |config|
-      expect(config.redis[:url]).to eq('redis://test-redis:6379/1')
-      expect(config.redis[:size]).to eq(10)
+  context "when custom Redis URL is set" do
+    before do
+      ENV['REDIS_URL'] = 'redis://custom-redis:6379/2'
+      ENV['SIDEKIQ_REDIS_POOL_SIZE'] = '10'
+    end
+
+    after do
+      ENV.delete('REDIS_URL')
+      ENV.delete('SIDEKIQ_REDIS_POOL_SIZE')
+    end
+
+    it "uses custom Redis configuration" do
+      Sidekiq.configure_server do |config|
+        expect(config.redis[:url]).to eq('redis://custom-redis:6379/2')
+        expect(config.redis[:size]).to eq(10)
+      end
     end
   end
 end
