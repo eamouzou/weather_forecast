1a51ac60e28f69a880da6b0f67d636739e7569fa
eamouzou <elom1990@yahoo.com>
2025-03-06 14:59:52 -0700

    Remove optional sidekiq middleware

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -7,11 +7,6 @@ Sidekiq.configure_server do |config|
     url: ENV.fetch("REDIS_URL") { "redis://localhost:6379/1" },
     size: ENV.fetch("SIDEKIQ_REDIS_POOL_SIZE") { 5 }
   }
-
-  # Optional: Configure server middleware
-  config.server_middleware do |chain|
-    chain.add Sidekiq::Middleware::Server::RetryJobs
-  end
 end
 
 Sidekiq.configure_client do |config|
